02af0f987600a696267c6ffe3a0ca1d0f577aabd
Navyendhu <navyendhummenon@gmail.com>
2024-09-12 11:08:08 +0530

    Queue

---
+++ Week 2/QUEUE/ex1.js
@@ -0,0 +1,39 @@
+
+
+class Queue {
+
+    constructor(){
+        this.items =[]
+    }
+
+
+    enqueue (ele){
+        return this.items.push(ele)
+    }
+
+    dequeue (){
+        return this.items.shift()
+    }
+
+    isEmpty(){
+        return this.items.length===0
+    }
+
+    peek(){
+        if (!this.isEmpty()){
+            return this.items[0]
+        }
+
+        return null
+    }
+   
+    size(){
+        this.items.length
+    }
+
+    print(){
+        console.log(this.items.toString(fi))
+    }
+
+
+}
